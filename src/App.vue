b3cf6ea292332513daf2960d06e93b2d6356898a
tomb <tomb@qq.com>
2019-08-27 11:37:18 +0800

    vuex1

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- 在组件对象中有一个$store对象，具体内容打印一下this查看-->
     <h1>次数：{{$store.state.count}},奇数or偶数：{{$store.getters.eventOrodd}}</h1>
     <button @click="increment">+</button>
     <button @click="decrement">—</button>
@@ -10,25 +11,17 @@
 
 <script>
 export default {
-  data() {
-    return {
-      count: 0
-    }
-  },
   name: 'App',
-  computed: {
-    eventOrodd() {
-      return this.count % 2 === 0 ? '偶数' : '奇数'
-    }
-  },
   methods: {
     increment() {
+      // 通过$store对象上的commit提交一个mutation
       this.$store.commit('INCREMENT')
     },
     decrement() {
       this.$store.commit('DECREMENT')
     },
     oddIncrement() {
+      // 通过$store对象上的dispatch提交一个action
       this.$store.dispatch('oddIncrement')
     },
     asyncIncrement() {
